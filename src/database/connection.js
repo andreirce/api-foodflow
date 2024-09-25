ec9441e12de59fe088d511e838cd4ef9663fcf55
Andrei Levi <andreilevi21@gmail.com>
2024-09-25 01:38:52 -0300

    add: adding port

--- src/database/connection.js
+++ src/database/connection.js
@@ -9,7 +9,8 @@ export const database = new Sequelize(
     process.env.DB_PASS,
     {
         host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT || 3306,
     }
 );
 
